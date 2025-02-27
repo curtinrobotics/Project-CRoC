def18594148ec4a771b4e93f1916e21731c72a18
Sheep903 <s.l.giacomel@gmail.com>
2025-02-27 23:14:55 +0800

    Added LED Matrix Sinage folder and testing code

---
+++ LED Matrix Sigange/FunctioningExample/FunctioningExample.ino
@@ -0,0 +1,166 @@
+/*
+Code based of examples from Adafruit library
+https://github.com/adafruit/RGB-matrix-Panel/tree/master
+
+NOTE: This codes works; it has been tested; DO NOT CHANGE IT
+*/
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include "RGBmatrixPanel.h" // Hardware-specific library
+
+// Connect ALL pins listed below to an Arduino Mega
+#define CLK 11
+#define OE  9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+// R1: 24
+// G1: 25
+// B1: 26
+// R2: 27
+// G2: 28
+// B2: 29
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+
+void setup() {
+
+  matrix.begin();
+
+  adaFruitIntro();  // From examples/testshapes_32x64
+  delay(2000);
+  colorWheel();  // From exampes/colorwheel_32x32
+
+}
+
+void loop() {
+    
+  
+}
+
+void colorWheel(void)
+{
+  int      x, y, hue;
+  float    dx, dy, d;
+  uint8_t  sat, val;
+  uint16_t c;
+
+  for(y=0; y < matrix.width(); y++) {
+    dy = 15.5 - (float)y;
+    for(x=0; x < matrix.height(); x++) {
+      dx = 15.5 - (float)x;
+      d  = dx * dx + dy * dy;
+      if(d <= (16.5 * 16.5)) { // Inside the circle(ish)?
+        hue = (int)((atan2(-dy, dx) + PI) * 1536.0 / (PI * 2.0));
+        d = sqrt(d);
+        if(d > 15.5) {
+          // Do a little pseudo anti-aliasing along perimeter
+          sat = 255;
+          val = (int)((1.0 - (d - 15.5)) * 255.0 + 0.5);
+        } else
+        {
+          // White at center
+          sat = (int)(d / 15.5 * 255.0 + 0.5);
+          val = 255;
+        }
+        c = matrix.ColorHSV(hue, sat, val, true);
+      } else {
+        c = 0;
+      }
+      matrix.drawPixel(x, y, c);
+      matrix.drawPixel(x+32, y, c);
+    }
+  }
+}
+
+void adaFruitIntro(void)
+{
+  // draw a pixel in solid white
+  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
+  delay(500);
+
+  // fix the screen with green
+  matrix.fillRect(0, 0, matrix.width(), matrix.height(), matrix.Color333(0, 7, 0));
+  delay(500);
+
+  // draw a box in yellow
+  matrix.drawRect(0, 0, matrix.width(), matrix.height(), matrix.Color333(7, 7, 0));
+  delay(500);
+
+  // draw an 'X' in red
+  matrix.drawLine(0, 0, matrix.width()-1, matrix.height()-1, matrix.Color333(7, 0, 0));
+  matrix.drawLine(matrix.width()-1, 0, 0, matrix.height()-1, matrix.Color333(7, 0, 0));
+  delay(500);
+
+  // draw a blue circle
+  matrix.drawCircle(10, 10, 10, matrix.Color333(0, 0, 7));
+  delay(500);
+
+  // fill a violet circle
+  matrix.fillCircle(40, 21, 10, matrix.Color333(7, 0, 7));
+  delay(500);
+
+  // fill the screen with 'black'
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+
+  // draw some text!
+  matrix.setTextSize(1);     // size 1 == 8 pixels high
+  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+
+  matrix.setCursor(8, 0);    // start at top left, with 8 pixel of spacing
+  uint8_t w = 0;
+  char *str = "AdafruitIndustries";
+  for (w=0; w<8; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.setCursor(2, 8);    // next line
+  for (w=8; w<18; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.println();
+  //matrix.setTextColor(matrix.Color333(4,4,4));
+  //matrix.println("Industries");
+  matrix.setTextColor(matrix.Color333(7,7,7));
+  matrix.println("LED MATRIX!");
+
+  // print each letter with a rainbow color
+  matrix.setTextColor(matrix.Color333(7,0,0));
+  matrix.print('3');
+  matrix.setTextColor(matrix.Color333(7,4,0));
+  matrix.print('2');
+  matrix.setTextColor(matrix.Color333(7,7,0));
+  matrix.print('x');
+  matrix.setTextColor(matrix.Color333(4,7,0));
+  matrix.print('6');
+  matrix.setTextColor(matrix.Color333(0,7,0));
+  matrix.print('4');
+  matrix.setCursor(34, 24);
+  matrix.setTextColor(matrix.Color333(0,7,7));
+  matrix.print('*');
+  matrix.setTextColor(matrix.Color333(0,4,7));
+  matrix.print('R');
+  matrix.setTextColor(matrix.Color333(0,0,7));
+  matrix.print('G');
+  matrix.setTextColor(matrix.Color333(4,0,7));
+  matrix.print('B');
+  matrix.setTextColor(matrix.Color333(7,0,4));
+  matrix.print('*');
+}
+
+// Input a value 0 to 24 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint16_t Wheel(byte WheelPos) {
+  if(WheelPos < 8) {
+   return matrix.Color333(7 - WheelPos, WheelPos, 0);
+  } else if(WheelPos < 16) {
+   WheelPos -= 8;
+   return matrix.Color333(0, 7-WheelPos, WheelPos);
+  } else {
+   WheelPos -= 16;
+   return matrix.Color333(WheelPos, 0, 7 - WheelPos);
+  }
+}
